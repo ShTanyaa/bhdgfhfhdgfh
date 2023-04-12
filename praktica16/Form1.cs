3ca6d74634b3362f6a8fbd3a77b9d9343d81b904
ShTanyaa <124123694+ShTanyaa@users.noreply.github.com>
2023-04-12 09:57:55 +0500

    Add files via upload

---
+++ praktica16/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace praktica16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double y = 0;
+            for(int x=-360;x<=360;x++)
+            {
+                y = Math.Sin(Math.PI / 180 * x);
+                chart1.Series["синус"].Points.AddXY(x, y);
+                y = Math.Cos(Math.PI / 180 * x);
+                chart1.Series["косинус"].Points.AddXY(x, y);
+            }
+        }
+    }
+}
